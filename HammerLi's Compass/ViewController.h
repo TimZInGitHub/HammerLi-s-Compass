629a2644871f1d4ade40755b6d2169d9bb50a453
TimZInGitHub <Tim@11234.local>
2015-11-13 09:46:12 +0800

    Initial Commit

---
+++ HammerLi's Compass/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HammerLi's Compass
+//
+//  Created by Tim.Z on 11/13/15.
+//  Copyright © 2015 Tim.Z. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HammerLi's CompassTests/HammerLi_s_CompassTests.m
@@ -0,0 +1,39 @@
+//
+//  HammerLi_s_CompassTests.m
+//  HammerLi's CompassTests
+//
+//  Created by Tim.Z on 11/13/15.
+//  Copyright © 2015 Tim.Z. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface HammerLi_s_CompassTests : XCTestCase
+
+@end
+
+@implementation HammerLi_s_CompassTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
